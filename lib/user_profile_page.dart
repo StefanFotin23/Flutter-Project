1fe5de6c88e06a18829a3995532ae680326201a2
Stefan Fotin <andreifotin@gmail.com>
2024-01-28 19:04:29 +0200

    User Profile Page

--- lib/user_profile_page.dart
+++ lib/user_profile_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:toast/toast.dart';
 
 class UserProfilePage extends StatefulWidget {
   const UserProfilePage({Key? key});
@@ -14,6 +16,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
   String _firstName = '';
   String _lastName = '';
   String _phoneNumber = '';
+  String defaultProfilePictureURL = 'https://cdn2.vectorstock.com/i/1000x1000/20/76/man-avatar-profile-vector-21372076.jpg';
 
   @override
   void initState() {
@@ -53,7 +56,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
     }
   }
 
-  String _profilePictureUrl = 'https://example.com/default_profile_picture.jpg';
+  String _profilePictureUrl = 'https://cdn2.vectorstock.com/i/1000x1000/20/76/man-avatar-profile-vector-21372076.jpg';
 
   void _handleLogout() async {
     await _deleteCredentials();
@@ -66,23 +69,18 @@ class _UserProfilePageState extends State<UserProfilePage> {
     prefs.remove('password');
   }
 
-  void _handleChangeProfilePicture() {
-    setState(() {
-      _profilePictureUrl = 'https://example.com/new_profile_picture.jpg';
-    });
-  }
-
-  void _handleChangePhoneNumber() async {
-    final newPhoneNumber = await showDialog<String>(
+  Future<void> _handleChangeProfilePicture() async {
+    final newProfilePictureUrl = await showDialog<String>(
       context: context,
       builder: (BuildContext context) {
+        String? url;
         return AlertDialog(
-          title: const Text('Change Phone Number'),
+          title: const Text('Change Profile Picture'),
           content: TextField(
             onChanged: (value) {
-              _phoneNumber = value;
+              url = value;
             },
-            decoration: const InputDecoration(labelText: 'New Phone Number'),
+            decoration: const InputDecoration(labelText: 'New Profile Picture URL'),
           ),
           actions: <Widget>[
             TextButton(
@@ -93,11 +91,63 @@ class _UserProfilePageState extends State<UserProfilePage> {
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop(_phoneNumber); // Confirm
+                Navigator.of(context).pop(url); // Confirm
               },
               child: const Text('Confirm'),
             ),
           ],
+        );
+      },
+    );
+
+    if (newProfilePictureUrl != null) {
+      try {
+        final response = await http.head(Uri.parse(newProfilePictureUrl));
+
+        if (response.statusCode == 200) {
+          setState(() {
+            _profilePictureUrl = newProfilePictureUrl;
+          });
+        } else {
+          // Display a toast message for invalid image URL
+          Toast.show("Invalid image URL", duration: Toast.lengthShort, gravity: Toast.bottom);
+        }
+      } catch (e) {
+        // Handle errors and display a toast message
+        Toast.show("Error: $e", duration: Toast.lengthShort, gravity: Toast.bottom);
+      }
+    }
+  }
+
+  void _handleChangePhoneNumber() async {
+    final newPhoneNumber = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        String? phoneNumber;
+        return SingleChildScrollView(
+          child: AlertDialog(
+            title: const Text('Change Phone Number'),
+            content: TextField(
+              onChanged: (value) {
+                phoneNumber = value;
+              },
+              decoration: const InputDecoration(labelText: 'New Phone Number'),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(null); // Cancel
+                },
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(phoneNumber); // Confirm
+                },
+                child: const Text('Confirm'),
+              ),
+            ],
+          ),
         );
       },
     );
@@ -124,45 +174,47 @@ class _UserProfilePageState extends State<UserProfilePage> {
       appBar: AppBar(
         title: const Text('User Profile'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 100.0,
-                backgroundImage: NetworkImage(_profilePictureUrl),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                _displayName,
-                style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                _displayPhoneNumber,
-                style: _phoneNumber.isNotEmpty
-                    ? const TextStyle(fontSize: 16.0)
-                    : const TextStyle(fontSize: 16.0, color: Colors.red),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _handleChangeProfilePicture,
-                child: const Text('Change Profile Picture'),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _handleChangePhoneNumber,
-                child: const Text('Change Phone Number'),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _handleLogout,
-                child: const Text('Logout'),
-              ),
-            ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: NetworkImage(_profilePictureUrl),
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  _displayName,
+                  style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  _displayPhoneNumber,
+                  style: _phoneNumber.isNotEmpty
+                      ? const TextStyle(fontSize: 16.0)
+                      : const TextStyle(fontSize: 16.0, color: Colors.red),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _handleChangeProfilePicture,
+                  child: const Text('Change Profile Picture'),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _handleChangePhoneNumber,
+                  child: const Text('Change Phone Number'),
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _handleLogout,
+                  child: const Text('Logout'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
